9e557d92b6d48205f553e702091b7d51379c823a
AlexFerry <53348760+AlexFerry@users.noreply.github.com>
2026-09-11 22:01:35 -0300

    Update event labels and activity types

--- events.js
+++ events.js
@@ -1,14 +1,14 @@
 addEventButton({
   id: "evento-1",
   label: {
-    pt: "Surpresa Embalada",
-    en: "Event 1",
+    pt: "Salvar a Nave Estelar",
+    en: "Save the Starship",
   },
   category: "cat_events",
 }, async function () {
   for (let chapter = 1; chapter <= 7; chapter++) {
     netManager.send("act.act_mini_game_start_c2s", {
-      act_type: 2112,
+      act_type: 4018,
       chapter_id: chapter,
       ext: [],
     });
@@ -16,7 +16,7 @@ addEventButton({
     await sleep(500);
 
     netManager.send("act.act_mini_game_result_c2s", {
-      act_type: 2112,
+      act_type: 4018,
       chapter_id: chapter,
       result: 103,
       ext: [],
@@ -25,7 +25,7 @@ addEventButton({
     await sleep(500);
   }
 });
-
+/*
 addEventButton({
   id: "evento-2",
   label: {
@@ -43,3 +43,4 @@ addEventButton({
     await sleep(300);
   }
 });
+*/
