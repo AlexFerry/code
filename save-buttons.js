c507f48d4d1a2bc0b071d021e37a300cef92867c
AlexFerry <53348760+AlexFerry@users.noreply.github.com>
2026-09-11 22:06:33 -0300

    xa

--- save-buttons.js
+++ save-buttons.js
@@ -1,1 +1,18 @@
+// pig button
+addEventButton({
+  id: "evento-2",
+  label: {
+    pt: "Esconde-esconde",
+    en: "Hide and seek",
+  },
+  category: "cat_events",
+}, async function () {
+  for (let pig = 140; pig <= 146; pig++) {
+    netManager.send("act.act_autumn_pig_find_c2s", {
+      act_type: 2113,
+      pig_id: pig,
+    });
 
+    await sleep(300);
+  }
+});
