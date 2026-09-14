d1710e27427c110113d73e46347f10bd76d9ebf2
AlexFerry <53348760+AlexFerry@users.noreply.github.com>
2026-09-14 17:14:57 -0300

    x

--- events.js
+++ events.js
@@ -26,3 +26,24 @@ addEventButton({
   }
 });
 
+addEventButton({
+  id: "evento-2",
+  label: {
+    pt: "Desafio de Saltos",
+    en: "Jumping Challenge",
+  },
+  category: "cat_events",
+}, async function () {
+  for (let chapter = 1; chapter <= 7; chapter++) {
+
+    netManager.send("act.act_seven_trial_angry_bird_result_c2s", {
+      act_type: 73,
+      chapter_id: chapter,
+      result: 1,
+      step:1,
+    });
+
+    await sleep(500);
+  }
+});
+
